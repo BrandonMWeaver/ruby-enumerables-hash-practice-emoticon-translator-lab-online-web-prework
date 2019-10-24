8bab6179a52b1777a80750f0469a52bc9b2a8fae
Brandon Weaver <a7xncob@gmail.com>
2019-10-24 00:59:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,9 +17,8 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   hash = load_library(file_path)
-  
-  hash[:get_emoticon].each do |item|
-    puts item
+  if emoticon == hash[:get_emoticon][emoticon]
+    return hash[:get_emoticon][emoticon]
   end
   
   return "Sorry, that emoticon was not found"
